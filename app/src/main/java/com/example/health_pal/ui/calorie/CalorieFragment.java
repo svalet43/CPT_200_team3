4797c92f2695f3cb7ec7879e1db8c87617adf1c6
svalet43 <stevenv17@yahoo.com>
2025-05-04 16:47:46 -0500

    button functionality for logs

--- app/src/main/java/com/example/health_pal/ui/calorie/CalorieFragment.java
+++ app/src/main/java/com/example/health_pal/ui/calorie/CalorieFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -29,7 +30,21 @@ public class CalorieFragment extends Fragment {
         View root = binding.getRoot();
 
         NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
-        //navController.navigate(R.id.action_nav_calorie_to_nav_dash);
+        //click listeners
+        Button btStat = binding.btHw;
+        btStat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_nav_calorie_to_nav_stat_log);
+            }
+        });
+        Button btNutri = binding.btNutrition;
+        btNutri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_nav_calorie_to_nav_food_log);
+            }
+        });
         
         return root;
     }
---
+++ app/src/main/java/com/example/health_pal/ui/calorie/StatLogFragment.java
@@ -0,0 +1,31 @@
+package com.example.health_pal.ui.calorie;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.health_pal.R;
+import com.example.health_pal.databinding.FragNutrilogBinding;
+import com.example.health_pal.databinding.FragStatLogBinding;
+
+public class StatLogFragment extends Fragment {
+    private FragStatLogBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragStatLogBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+        //navController.navigate(R.id.action_nav_calorie_to_nav_dash);
+        return root;
+    }
+}
