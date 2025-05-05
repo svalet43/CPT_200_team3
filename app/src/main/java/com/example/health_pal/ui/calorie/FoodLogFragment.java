d05781813dc3978fe0c2746d7ef193eaa253a016
svalet43 <stevenv17@yahoo.com>
2025-05-04 20:38:31 -0500

    Partial functionality for log fragments

--- app/src/main/java/com/example/health_pal/ui/calorie/FoodLogFragment.java
+++ app/src/main/java/com/example/health_pal/ui/calorie/FoodLogFragment.java
@@ -9,6 +9,8 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.health_pal.R;
 import com.example.health_pal.databinding.FragLogIntroBinding;
@@ -16,6 +18,7 @@ import com.example.health_pal.databinding.FragNutrilogBinding;
 
 public class FoodLogFragment extends Fragment {
     private FragNutrilogBinding binding;
+    EditText etCal, etPro, etFat, etCarb;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +28,23 @@ public class FoodLogFragment extends Fragment {
         View root = binding.getRoot();
 
         NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
-        //navController.navigate(R.id.action_nav_calorie_to_nav_dash);
+        etCal = binding.ETCal;
+        etPro = binding.ETPro;
+        etFat = binding.ETFat;
+        etCarb = binding.ETCarb;
+
+        //click listener
+        Button btSub = binding.btSubmitNutri;
+        btSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if day is in database
+                //if yes pull data and add new data
+                //if not create new day and push to database
+                //navigate back to dashboard
+                navController.navigate(R.id.action_nav_food_log_to_nav_dash);
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/example/health_pal/ui/calorie/StatLogFragment.java
+++ app/src/main/java/com/example/health_pal/ui/calorie/StatLogFragment.java
@@ -9,6 +9,8 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.health_pal.R;
 import com.example.health_pal.databinding.FragNutrilogBinding;
@@ -16,6 +18,7 @@ import com.example.health_pal.databinding.FragStatLogBinding;
 
 public class StatLogFragment extends Fragment {
     private FragStatLogBinding binding;
+    EditText etFt, etIn, etWeight;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +28,22 @@ public class StatLogFragment extends Fragment {
         View root = binding.getRoot();
 
         NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
-        //navController.navigate(R.id.action_nav_calorie_to_nav_dash);
+        etFt = binding.ETFt;
+        etIn = binding.ETIn;
+        etWeight = binding.ETWeight;
+
+        Button btSub = binding.btSubmitStat;
+        btSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if day is in database
+                //if yes pull data and add new data
+                //if not create new day and push to database
+                //navigate back to dashboard
+                navController.navigate(R.id.action_nav_stat_log_to_nav_dash);
+            }
+        });
+
         return root;
     }
 }
